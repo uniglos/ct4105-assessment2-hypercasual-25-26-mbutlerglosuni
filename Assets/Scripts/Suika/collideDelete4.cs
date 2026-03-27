43a4adac5faa7734bc0fa6217521d61bac76de26
MayaGlosUni <s4541150@glos.ac.uk>
2026-03-27 13:19:42 +0000

    they delete when they collide with themself

---
+++ Assets/Scripts/Suika/collideDelete4.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class collideDelete4 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "fish4")
+        {
+            Debug.Log("collided");
+            Destroy(gameObject);
+        }
+    }
+}
